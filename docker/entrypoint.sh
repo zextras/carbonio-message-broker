0ccab916f4a1e1090d35884e0c8711a02d339cdb
Davide Frison <davide.frison@zextras.com>
2026-04-10 14:38:43 +0200

    chore(docker): by default do not run setup (retro compatible with docker compose)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-echo "[message-broker] Waiting for consul agent..."
-until consul members >/dev/null 2>&1; do sleep 1; done
-echo "[message-broker] Consul agent ready."
+if [ "${CONSUL_SETUP}" != "false" ]; then
+  echo "[message-broker] Waiting for consul agent..."
+  until consul members >/dev/null 2>&1; do sleep 1; done
+  echo "[message-broker] Consul agent ready."
 
-# Wait for RabbitMQ to be ready, then run setup-users in background
-(
-  echo "Waiting for RabbitMQ to be ready..."
-  until rabbitmqctl status >/dev/null 2>&1; do sleep 2; done
-  echo "RabbitMQ is ready, running setup-users..."
-  carbonio-message-broker-setup-users
-) &
+  # Wait for RabbitMQ to be ready, then run setup-users in background
+  (
+    echo "Waiting for RabbitMQ to be ready..."
+    until rabbitmqctl status >/dev/null 2>&1; do sleep 2; done
+    echo "RabbitMQ is ready, running setup-users..."
+    carbonio-message-broker-setup-users
+  ) &
+fi
 
 # Exec into the original RabbitMQ entrypoint
 exec docker-entrypoint.sh rabbitmq-server
